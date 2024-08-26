5db7663514834013e7e30fdcce1b26ef52dde2d5
wzalaz4r <167816702+wzalaz4r@users.noreply.github.com>
2024-08-26 11:56:17 -0400

    Solve 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+	// return parseInt(nums.reduce((total, n) => total + n, 0));
+  return nums.reduce((total, num) => (total) + (num), 0);
+};
+const multiply = function(nums) {
+  return nums.reduce((total, num) => total * num, 1);
 };
 
-const multiply = function() {
-
+const power = function(n,e) {
+	return n**e;
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(n) {
+	for (let i = n - 1; i > 0; i--) {
+    n *= i
+  }
+  // Math.factorial(n);
+  return n==0 ? 1 : n;
 };
 
 // Do not edit below this line
